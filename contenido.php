3bc7657cc0bc161c3e0a864fb7793e560b752432
SaamMj12a <72233284+SaamMj12a@users.noreply.github.com>
2020-10-02 02:06:01 -0500

    Add files via upload

---
+++ contenido.php
@@ -0,0 +1,28 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/el.jpg"."||".
+				  "Acaricia a tu perro"."||".
+				  "Un trato amoroso es muy aconsejable para la salud de tu mascota. Tómate algún momento del día para compartir con tu perro y consigue un tiempo de calidad. Aprovecha para acariciarlo, abrazarlo o darle un masaje relajante en patas y muslos.";
+
+		$datos[1]="img/smile.jpg"."||".
+				  "Premia su buen comportamiento"."||".
+				  "Cuanto tu perro haga algo que le pediste o responda a cierto tipo de entrenamiento, felicítalo y premialo ya sea con golosinas, caricias o juguetes.";
+
+		$datos[2]="img/wow.jpg"."||".
+				  "Educa a tu perro"."||".
+				  "En la educación de la mascota se debe incluir: horarios de comidas, de salidas, espacios donde puede dormir, elementos con los que puede jugar, el lugar donde hacer sus necesidades, etc. Que aprenda todas estas cosas garantiza una buena adaptación del perro al hogar.";
+
+		$datos[3]="img/ojo.jpg"."||".
+				  "Visita al veterinario"."||".
+				  "Dentro de los cuidados que necesita un animal son importantes las visitas regulares al veterinario donde le pongan las vacunas y el chip obligatorio.";
+
+		$datos[4]="img/perro.jpg"."||".
+				  "Alimentación"."||".
+				  "Es importante que los perros sigan una buena alimentación. Hablar con el veterinario nos ayudará a saber los nutrientes necesarios para nuestra mascota según su raza, condición física y peso.";
+		return $datos;
+	}
+
+ ?>
